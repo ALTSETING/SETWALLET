62829a7b63d024c305eb470ee93fa992c56ff808
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2026-01-03 15:50:04 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,8 @@
 
+from fastapi import FastAPI
+from app.routes import wallets, transactions
+
+app = FastAPI(title="SETWALLET OnlyChain")
+
+app.include_router(wallets.router)
+app.include_router(transactions.router)

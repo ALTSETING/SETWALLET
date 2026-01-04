9c82d4ab9ee393aabce2334898567e257f47e0d4
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2026-01-04 06:46:17 +0100

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-NETWORK_ID = os.getenv("NETWORK_ID", "SETONLYCHAIN")
-
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
+
+NETWORK_ID = os.getenv("NETWORK_ID", "SETONLYCHAIN")
